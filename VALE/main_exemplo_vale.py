37b3954aaee83c3563b1231789b728ed2f5f2886
bambirra <bambirrarafaelp@gmail.com>
2023-03-08 16:52:48 -0300

    implementacao otimizacao

--- VALE/main_exemplo_vale.py
+++ VALE/main_exemplo_vale.py
@@ -1,28 +1,52 @@
 import numpy as np
-import pandas as pd
 import numpy_financial as npf
+import pandas as pd
+import scipy.optimize as opt
 from portfolio import geracao_hidro, geracao_solar, geracao_eolica, compras, vendas, preco_compras, preco_vendas, carga_total
 from cenarios import df_cenario_base, df_cenario_pouca_chuva, df_cenario_desenvolvimento_mundial, df_cenario_incentivo_renovaveis
 
 
-def calc_receita(x_contrat, x_hidro, x_solar, x_eolico):
-    exp = calc_exposicao(x_contrat, x_hidro, x_solar, x_eolico)
-    return 8760 * (vendas * preco_vendas + pld * exp - compras * preco_compras - custo_om_hidro * geracao_hidro - custo_om_solar * geracao_solar - p_contrat * x_contrat - p_hidro * x_hidro - p_solar * x_solar - p_eolico * x_eolico)
+def calc_vpl(x):
+    x_reshaped = x.reshape([4, t])
+    x_contrat = x_reshaped[0]
+    x_hidro = x_reshaped[1]
+    x_solar = x_reshaped[2]
+    x_eolico = x_reshaped[3]
+    exp = calc_exposicao(x)
+    receita_financeira = 8760 * (vendas * preco_vendas + pld * exp - compras * preco_compras - custo_om_hidro * geracao_hidro - custo_om_solar * geracao_solar - p_contrat * x_contrat - p_hidro * x_hidro - p_solar * x_solar - p_eolico * x_eolico)
+    return npf.npv(rate=taxa_desconto, values=receita_financeira)
 
 
-def calc_risco(x_contrat, x_hidro, x_solar, x_eolico):
-    exp = calc_exposicao(x_contrat, x_hidro, x_solar, x_eolico)
-    return np.mean( abs(sigma_spot*exp) + abs(sigma_c*(x_contrat + compras)) + sigma_h*(x_hidro + geracao_hidro) + sigma_w*(x_eolico+geracao_eolica) + sigma_s*(x_solar + geracao_solar) )
+def calc_risco(x):
+    x_reshaped = x.reshape([4, t])
+    x_contrat = x_reshaped[0]
+    x_hidro = x_reshaped[1]
+    x_solar = x_reshaped[2]
+    x_eolico = x_reshaped[3]
+    exp = calc_exposicao(x)
+    return np.mean(abs(sigma_spot*exp) + abs(sigma_c*(x_contrat + compras)) + sigma_h*(x_hidro + geracao_hidro) + sigma_w*(x_eolico+geracao_eolica) + sigma_s*(x_solar + geracao_solar))
 
 
-def calc_exposicao(x_contrat, x_hidro, x_solar, x_eolico):
+def calc_exposicao(x):
+    x_reshaped = x.reshape([4, t])
+    x_contrat = x_reshaped[0]
+    x_hidro = x_reshaped[1]
+    x_solar = x_reshaped[2]
+    x_eolico = x_reshaped[3]
     return compras + geracao_hidro + geracao_solar + x_contrat + x_hidro + x_solar + x_eolico - vendas - carga_total
 
+def limite_inf_exposicao(x):
+    return -calc_exposicao(x) - limite_exposicao
+
+def limite_sup_exposicao(x):
+    return calc_exposicao(x) - limite_exposicao
 
 list_cenarios = [df_cenario_base, df_cenario_pouca_chuva, df_cenario_desenvolvimento_mundial, df_cenario_incentivo_renovaveis]
 
 t = 15
 taxa_desconto = 0.05
+limite_exposicao = 50
+
 lim_pld = [50, 600]
 lim_hidro = [-0.15, 0.1]
 lim_solar = [-0.1, 0.1]
@@ -36,7 +60,6 @@ x_contrat = np.zeros(t)
 x_hidro = np.zeros(t)
 x_solar = np.zeros(t)
 x_eolico = np.zeros(t)
-
 
 pld_fuzzy = np.zeros(t)
 gsf_hidro_fuzzy = np.zeros(t)
@@ -66,13 +89,29 @@ sigma_h = 0.2
 sigma_w = 0.2
 sigma_s = 0.2
 
+x0 = np.zeros(t*4)
+
+# x_reshaped = x.reshape([4, t])
+# x_contrat = x_reshaped[0]
+# x_hidro = x_reshaped[1]
+# x_solar = x_reshaped[2]
+# x_eolico = x_reshaped[3]
 
 
+# exp = calc_exposicao(x_contrat, x_hidro, x_solar, x_eolico)
+# vpl_receita = calc_vpl(x)
+# risco = calc_risco(x)
+limites_inferiores_variaveis = [-100]*t + list(-geracao_hidro) + list(-geracao_solar) + list(-geracao_eolica)
+limites_superiores_variaveis = [100]*4*t
+list_bounds = []
+for i in range(4*t):
+    list_bounds.append([limites_inferiores_variaveis[i], limites_superiores_variaveis[i]])
+list_constraint = [
+    {'type': 'ineq', 'fun': limite_inf_exposicao},
+    {'type': 'ineq', 'fun': limite_sup_exposicao}
+]
 
-exp = calc_exposicao(x_contrat, x_hidro, x_solar, x_eolico)
+result = opt.minimize(calc_vpl, x0, bounds=list_bounds)#, constraints=list_constraint)
 
-receita_financeira = calc_receita(x_contrat, x_hidro, x_solar, x_eolico)
-
-vpl = npf.npv(rate=taxa_desconto, values=receita_financeira)
 
 print("fim")
